99ea1c5a4db912e96c932142b2d547996f891ff4
ThiagoMarques <marques.thiago137@gmail.com>
2024-05-10 19:05:02 -0300

    Criação do cadastro de conta

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        numero  = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        agencia  = scanner.next();
+
+        System.out.println("Por favor, digite o seu nome: ");
+        nomeCliente  = scanner.next();
+
+        System.out.println("Por favor, digite o Saldo Inicial: ");
+        saldo  = scanner.nextDouble();
+
+        System.out.println("Olá, ".concat(nomeCliente)+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque.");
+
+        scanner.close();
+
     }
 }
